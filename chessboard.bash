6be89c5d098e0faa282259a57aac4824ee105c4d
priya23sharma34 <140498685+priya23sharma34@users.noreply.github.com>
2023-07-26 17:38:07 +0530

    Update chessboard.bash

--- chessboard.bash
+++ chessboard.bash
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Function to print the chessboard
-print_chessboard() {
-    for ((i = 1; i <= 8; i++)); do
-        for ((j = 1; j <= 8; j++)); do
+print_chess() {
+    for ((row= 1; row <= 8; row+)); do #this for loop is for iterating over the rows
+        for ((col = 1; col <= 8; col++)); do #this for loop is for iterating over the columns
             if (( (i + j) % 2 == 0 )); then
-                echo -n "██"
+                echo -ne "\e[47m " #this is for printing a white cell
             else
-                echo -n "  "
+                echo -ne "\e[40m " #this is for printing a black cell
             fi
         done
-        echo    # Move to the next line
+        echo -e "\e[0m " # set the text color to default and move to the next line
     done
 }
 
 # Call the function to print the chessboard
-print_chessboard
+print_ches
